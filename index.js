16b11fde5128d3c15ba6182dddf091067c05d77d
Ross <tom@umpox.com>
2017-09-29 16:14:22 +0100

    Add category value

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const googleTrends = require('./google-trends-api.min.js');
 
 const aWeekAgo = new Date();
-aWeekAgo.setDate(aWeekAgo.getDate() - 4);
+aWeekAgo.setDate(aWeekAgo.getDate() - 5);
 const dateMsg = aWeekAgo.getFullYear() + '-' + (aWeekAgo.getMonth()+1) + '-' + aWeekAgo.getDate();
 
 googleTrends.relatedQueries({
   keyword: ' ',
   startTime: new Date(dateMsg),
-  geo: 'GB'
+  category: 0
 })
 .then((res) => {
   const data = JSON.parse(res);
