ff8db20077fd974389aa126db79220dc13e337cf
Ross <tom@umpox.com>
2017-09-29 19:14:15 +0100

    Refactor into function and then merge objects

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
 const googleTrends = require('./google-trends-api.min.js');
 
-const aWeekAgo = new Date();
-aWeekAgo.setDate(aWeekAgo.getDate() - 5);
-const dateMsg = aWeekAgo.getFullYear() + '-' + (aWeekAgo.getMonth()+1) + '-' + aWeekAgo.getDate();
+function daysAgo(days) {
+  const previousDate = new Date();
+  previousDate.setDate(previousDate.getDate() - days);
+  const dateMsg = previousDate.getFullYear() + '-' + (previousDate.getMonth()+1) + '-' + previousDate.getDate();
+  return new Date(dateMsg);
+};
 
-googleTrends.relatedQueries({
-  keyword: ' ',
-  startTime: new Date(dateMsg),
-  geo: 'GB',
-  category: 0
-})
-.then((res) => {
-  const data = JSON.parse(res);
+function sortByRising(data) {
+  data = Object.keys(data).sort(function(a,b){return data[a]-data[b]})
+  return data;
+}
+
+async function getTrends(
+  keyword = ' ',
+  days = 7,
+  category = 0
+) {
+  let queryRes;
+  try {
+    queryRes = await googleTrends.relatedQueries({
+      keyword: keyword,
+      startTime: daysAgo(days),
+      geo: 'GB',
+      category: category
+    })
+    queryRes = JSON.parse(queryRes);
+  } catch (e) {
+    console.log('Error:' + e);
+  } finally {
+    return queryRes;
+  }
+}
+
+async function main() {
+  let data1 = await getTrends(' ', 3, 3);
+  let data2 = await getTrends(' ', 3, 44)
+
+  const data = Object.assign(data1, data2);
+
   console.log(data.default.rankedList[1].rankedKeyword);
-})
-.catch((err) => {
-  console.log(err);
-});
+}
+main();
+
+// data = sortByRising(data);
+// console.log(data);
