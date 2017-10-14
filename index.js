3b8a47d91b160de3d8abd3a450cc2ad403ece761
Tom Ross <tom@umpox.com>
2017-10-14 19:47:34 +0100

    Add logic for multiple categories and removing duplicates

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
 const googleTrends = require('./google-trends-api.min.js');
+const fs = require('fs');
+
+const categories = {
+  ArtsEntertainment: 3,
+  // AutosVehicles: 47,
+  // BeautyFitness: 44,
+  // BooksLiterature: 22,
+  // BusinessIndustrial: 12,
+  // ComputersElectronics: 5,
+  // Finance: 7,
+  // FoodDrink: 71,
+  // Games: 8,
+  // Health: 45,
+  // HobbiesLeisure: 65,
+  // HomeGarden: 11,
+  // InternetTelecom: 13,
+  // JobsEducation: 958,
+  // LawGovernment: 19,
+  // News: 16,
+  // OnlineCommunities: 299,
+  // PeopleSociety: 14,
+  // PetsAnimals: 66,
+  // RealEstate: 29,
+  // Reference: 533,
+  // Science: 174,
+  // Shopping: 18,
+  // Sports: 20,
+  // Travel: 67
+};
 
 function daysAgo(days) {
   const previousDate = new Date();
@@ -7,9 +36,29 @@ function daysAgo(days) {
   return new Date(dateMsg);
 };
 
+function findQuery(data, type, value) {
+  return data[type] === value;
+}
+
+function removeDuplicates(data, compareObj) {
+  for (key in compareObj) {
+    //Search original object for duplicates
+    let duplicateFound = findDuplicate(data, compareObj[key].query)
+
+    if (duplicateFound[0]) {
+      delete data[duplicateFound[0].key];
+    }
+  }
+}
+
+function findDuplicate(data, value) {
+  //Find duplicate in other object
+  return data.filter(data => (data.query === value));  
+}
+
 function sortByRising(data) {
-  data = Object.keys(data).sort(function(a,b){return data[a]-data[b]})
-  return data;
+  //Sort by the % change value for rising trends
+  return data.sort((obj1, obj2) => obj2.change - obj1.change);
 }
 
 async function getTrends(
@@ -19,12 +68,15 @@ async function getTrends(
 ) {
   let queryRes;
   try {
+    //Call Google Trends API
     queryRes = await googleTrends.relatedQueries({
       keyword: keyword,
       startTime: daysAgo(days),
       geo: 'GB',
       category: category
     })
+
+    //Parse in JSON to use as object
     queryRes = JSON.parse(queryRes);
   } catch (e) {
     console.log('Error:' + e);
@@ -34,14 +86,58 @@ async function getTrends(
 }
 
 async function main() {
-  let data1 = await getTrends(' ', 3, 3);
-  let data2 = await getTrends(' ', 3, 44)
+  let overallData = [];
+  let data = {};
 
-  const data = Object.assign(data1, data2);
+  for (cat in categories) {
+    console.log(`trying to get data for ${cat}`);
 
-  console.log(data.default.rankedList[1].rankedKeyword);
+    //Query Google API for data
+    data = await getTrends(' ', 2, categories[cat]);
+    
+    //Focus data on only rising searches
+    data = data.default.rankedList[1].rankedKeyword      
+
+    //Loop through data and add it to the overallData object
+    Object.entries(data).forEach(([key, value], index) => {
+      let query = value.query;
+      let change = value.value;
+      let formattedChange = value.formattedValue;
+      index = Object.keys(overallData).length + index;
+
+      overallData.push({
+        index,
+        query,
+        change,
+        formattedChange
+      });
+    });
+
+    //Reset data
+    data = {};
+  }
+
+  return overallData;
 }
-main();
 
-// data = sortByRising(data);
-// console.log(data);
+(async code => {
+  //Get previous dates data
+  let oldSearches = [];
+  oldSearches = fs.readFileSync('data.json','utf8');
+
+  //Get todays data
+  risingSearches = await main();
+  risingSearches = sortByRising(risingSearches);
+
+  //Remove duplicates between previous date and today
+  removeDuplicates(risingSearches, oldSearches);
+
+
+  fs.writeFile('data.json', JSON.stringify(risingSearches), () => {  
+    // success case, the file was saved
+    console.log('Data saved!');
+  });
+})();
+
+
+// TODO FOR SOME REASON THE KEYS AREN'T UPDATING?
